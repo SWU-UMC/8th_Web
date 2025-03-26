e6b0034007d3435cdca0c27785bc03e11e226bfa
Minkyung02 <minkyung22@swu.ac.kr>
2025-03-26 10:57:02 +0900

    Feat: component 분리 및 Context API 활용해서 기능 구현

--- 해밍_황민경/2주차 미션/미션 1/src/App.tsx
+++ 해밍_황민경/2주차 미션/미션 1/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Todo from "./components/Todo";
+import { TodoProvider } from "./context/TodoContext";
 
 function App(): Element {
   return (
-    <>
+    <TodoProvider>
       <Todo />
-    </>
+    </TodoProvider>
   );
 }
 
