236c8113f34d8cfc8bb4fe48019946fcb415457a
Minkyung02 <minkyung22@swu.ac.kr>
2025-04-08 10:51:53 +0900

    Feat: 로그인 유효성 기능 구현하기

--- w4-m2/src/App.tsx
+++ w4-m2/src/App.tsx
@@ -1,7 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import HomeLayout from "./layouts/HomeLayout.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import SignupPage from "./pages/SignupPage.tsx";
+
+const router: Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
