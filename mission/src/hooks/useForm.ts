e0c1bdbdcc9fa3de3e2b982dac042b78161bf012
Minkyung02 <minkyung22@swu.ac.kr>
2025-05-06 06:31:54 +0900

    Feat: Lp데이터 불러오기와 무한 스크롤 기능 구현하기

---
+++ mission/src/hooks/useForm.ts
@@ -0,0 +1,45 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
+interface useFormProps<T> {
+  initialValue: T;
+  validate: (values: T) => Record<keyof T, string>;
+}
+
+function useForm<T>({ initialValue, validate }: useFormProps<T>) {
+  const [values, setValues] = useState(initialValue);
+  const [touched, setTouched] = useState<Record<string, boolean>>();
+  const [errors, setErrors] = useState<Record<string, string>>();
+
+  //사용자가 입력값을 바꿀 때 실행되는 함수
+  const handleChange = (name: keyof T, text: string) => {
+    setValues({
+      ...values, //불변성 유지(기존 값 유지)
+      [name]: text,
+    });
+  };
+
+  const handleBlur = (name: keyof T) => {
+    setTouched({
+      ...touched, //불변성 유지(기존 값 유지)
+      [name]: true,
+    });
+  };
+
+  const getInputProps = (name: keyof T) => {
+    const value = values[name];
+    const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+      handleChange(name, e.target.value);
+    const onBlur = () => handleBlur(name);
+
+    return { value, onChange, onBlur };
+  };
+
+  useEffect(() => {
+    const newErrors = validate(values);
+    setErrors(newErrors);
+  }, [validate, values]);
+
+  return { values, errors, touched, getInputProps };
+}
+
+export default useForm;
