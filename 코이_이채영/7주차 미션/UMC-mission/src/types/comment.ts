c9bc6b682a7bcdc9e43682af1ea7e0e68eb45ca0
chae1125 <y72682685@gmail.com>
2025-05-14 13:27:27 +0900

    Feat: useMutation과 Optimistic Update를 이용해 LP와 댓글 생성, 수정, 삭제 및 내 정보 수정하기

---
+++ 코이_이채영/7주차 미션/UMC-mission/src/types/comment.ts
@@ -0,0 +1,47 @@
+// types/comment.ts
+
+export interface CommentAuthor {
+    id: number;
+    name: string;
+    email: string;
+    bio: string | null;
+    avatar: string | null;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface Comment {
+    id: number;
+    content: string;
+    lpId: number;
+    authorId: number;
+    createdAt: string;
+    updatedAt: string;
+    author: CommentAuthor;
+}
+
+export interface ResponseCommentListDto {
+    status: boolean;
+    statusCode: number;
+    message: string;
+    data: {
+        data: Comment[];
+        nextCursor: number;
+        hasNext: boolean;
+    };
+}
+
+export interface RequestCommentDto {
+    lpId: number;
+    content: string;
+}
+
+export interface ResponseCommentDto {
+    id: number;
+    content: string;
+    lpId: number;
+    authorId: number;
+    createdAt: string;
+    updatedAt: string;
+    author: CommentAuthor;
+}
