f23aa7c3021ce2a6d9881ce8ae4582f3fcca9118
Minkyung02 <minkyung22@swu.ac.kr>
2025-06-04 11:52:52 +0900

    Delete: 불필요한 추적 삭제

--- w10_mission/mission/src/apis/axiosClient.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const axiosClient = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-  },
-});
--- w10_mission/mission/src/constants/movie.ts
+++
@@ -1,5 +0,0 @@
-export const LANGUAGE_OPTIONS = [
-  { value: "ko-KR", label: "한국어" },
-  { value: "en-US", label: "English" },
-  { value: "ja-JP", label: "日本語" },
-];
